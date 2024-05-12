f82a83d5395bc32d42a44accfbe8fd9813231f8b
exgraal7 <144822299+exgraal7@users.noreply.github.com>
2024-05-13 00:16:14 +0300

    Create solution.cpp

---
+++ 0007-reverse-integer/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+ public:
+  int reverse(int x) {
+    int reversedNumber = 0;
+
+    while (x != 0) {
+      if (reversedNumber < INT_MIN / 10 || reversedNumber > INT_MAX / 10) {
+        return 0;
+      }
+
+      reversedNumber = reversedNumber * 10 + x % 10;
+
+      x /= 10;
+    }
+
+    return reversedNumber;
+  }
+};
