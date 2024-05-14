7f05a862cdc3f6d38dc14f932a33c42ab037bccd
exgraal7 <144822299+exgraal7@users.noreply.github.com>
2024-05-14 20:01:15 +0300

    Create solution.java

---
+++ 0069-sqrtx/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int mySqrt(int x) {
+    if (x == 0)
+      return 0;
+    int start = 1, end = x / 2 + 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (mid == x / mid)
+        return mid;
+      else if (mid < x / mid)
+        start = mid + 1;
+      else
+        end = mid - 1;
+    }
+    return end;
+  }
+}
