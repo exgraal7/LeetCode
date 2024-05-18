963f79208e75f99322dc3c8bf77b4f6a6bbbacd5
exgraal7 <144822299+exgraal7@users.noreply.github.com>
2024-05-18 18:51:01 +0300

    Update and rename solution.py to solution.java

--- 0001-two-sum/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n_dict = {}
-        for i in range(len(nums)):
-            f_num = target - nums[i]
-            if n_dict.get(f_num) != None:
-                return [n_dict[f_num], i]
-            n_dict[nums[i]] = i
-        return []
